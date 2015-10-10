439860334aa94432bd0cac4d4b7ea57ce3a76cd8
Braam Genis <braamgenis@gmail.com>
2015-10-10 13:39:03 +0200

    Corrected some folders in build-config.js

--- build-config.js
+++ build-config.js
@@ -18,7 +18,7 @@ module.exports = {
     js: {
         src: [
             'js/**/*.js',
-            '!{node_modules,node_modules/**}',  // Ignore node_modules/ and contents
+            '!{build,build/**}',                // Ignore build/ and contents
             '!{tests,tests/**}',                // Ignore tests/ and contents
             '!{tmp,tmp/**}'                     // Ignore dist/ and contents
         ]
@@ -32,8 +32,7 @@ module.exports = {
             '!{tmp,tmp/**}',                    // Ignore tmp/ and contents
             '!phpunit.xml',                     // Not the unit tests configuration file.
             '!so-css.php',                      // Not the base plugin file. It is copied by the 'version' task.
-            '!readme.txt',                      // Not the readme.txt file. It is copied by the 'version' task.
-            '!package.json'                     // Not the package.json file.
+            '!readme.txt'                      // Not the readme.txt file. It is copied by the 'version' task.
         ]
     }
 };
