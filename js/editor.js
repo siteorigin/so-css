adf34eb76c14e2ed3f44983cf021c30f9177cdfa
Braam Genis <braam@siteorigin.com>
2017-05-23 17:26:45 +0200

    To resolve #32. Increment and decrement buttons work when value empty or zero. Also added repeating action while button held down.

--- js/editor.js
+++ js/editor.js
@@ -1481,32 +1481,46 @@
             var $dec = $('<div class="dec-button socss-button"><span class="fa fa-minus"></span></div>').appendTo($diw);
             var $inc = $('<div class="inc-button socss-button"><span class="fa fa-plus"></span></div>').appendTo($diw);
 
-            // Increment is clicked
-            $inc.click( function(){
-                var value = thisView.parseUnits( $el.val() );
-                if( value.value === '' ) {
-                    return true;
+            var stepValue = function( direction ) {
+                var value = Number.parseInt( thisView.parseUnits( $el.val() ).value );
+
+                if( Number.isNaN( value ) ) {
+                    value = 0;
                 }
 
-                var newVal = Math.ceil( value.value * 1.05 );
+                var newVal = value + direction;
 
                 $fi.val( newVal );
                 updateValue();
                 $el.trigger('change').trigger('measurement_refresh');
-            } );
+            };
 
-            $dec.click( function(){
-                var value = thisView.parseUnits( $el.val() );
-                if( value.value === '' ) {
-                    return true;
-                }
+            var setupStepButton = function ( $button ) {
+                var direction = $button.is( '.dec-button' ) ? -1 : 1;
+                var intervalId;
+                var timeoutId;
+                $button.mousedown( function(){
+                    stepValue( direction );
+                    timeoutId = setTimeout( function () {
+                        intervalId = setInterval( function () {
+                            stepValue( direction );
+                        }, 50 );
+                    }, 500 );
+                } ).on( 'mouseup mouseout', function(){
+                    if ( timeoutId ) {
+                        clearTimeout( timeoutId );
+                        timeoutId = null;
+                    }
+                    if ( intervalId ) {
+                        clearInterval( intervalId );
+                        intervalId = null;
+                    }
+                } );
+            };
 
-                var newVal = Math.floor( value.value / 1.05 );
+            setupStepButton( $dec );
+            setupStepButton( $inc );
 
-                $fi.val( newVal );
-                updateValue();
-                $el.trigger('change').trigger('measurement_refresh');
-            } );
         }
 
     } );
